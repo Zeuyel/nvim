55352884a825aef339778cc7adbc3eb87336fc53
wyz@epictus.com <wyz@epictus.com>
2024-08-28 15:27:31 +0800

    update the clipboard config for windows terminal or other osc_54

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,12 +6,26 @@ local function augroup(name)
   return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
 end
 
+-- Set scrolloff when entering insert mode
 vim.api.nvim_create_autocmd({ "InsertEnter" }, {
-  group = augroup("always_center"),
-  command = ":let &scrolloff = float2nr(floor(winheight(0)/2)",
+  group = augroup("insertcenter"),
+  pattern = "*",
+  callback = function()
+    local scrolloff_value = math.floor(vim.api.nvim_win_get_height(0) / 2)
+    vim.opt.scrolloff = scrolloff_value
+    -- Echo a message to confirm the autocmd was triggered
+    vim.api.nvim_echo({ { " new InsertEnter: Scrolloff set to " .. scrolloff_value, "Normal" } }, false, {})
+  end,
 })
 
+-- Set scrolloff when entering or resizing windows, or leaving insert mode
 vim.api.nvim_create_autocmd({ "VimEnter", "WinEnter", "VimResized", "InsertLeave" }, {
-  group = augroup("always_center"),
-  command = ":let &scrolloff = float2nr(floor(winheight(0)/2)+1)",
+  group = augroup("keep_center"),
+  pattern = "*",
+  callback = function()
+    local scrolloff_value = math.floor(vim.api.nvim_win_get_height(0) / 2) + 1
+    vim.g.scrolloff = scrolloff_value
+    -- Echo a message to confirm the autocmd was triggered
+    vim.api.nvim_echo({ { "Event triggered: Scrolloff set to " .. scrolloff_value, "Normal" } }, false, {})
+  end,
 })
